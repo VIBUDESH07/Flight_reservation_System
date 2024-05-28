a28f91196d6a617852d9c5c04bdcf46867cc8d74
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-28 22:54:34 +0530

    Update Header.jsx

--- dbms-flight/src/Components/Header.jsx
+++ dbms-flight/src/Components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faPlane, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import logoImage from '../Styles/pics/malediven[1].webp'; // Import your image
 import '../Styles/header.css';
 
 const Header = () => {
   return (
     <div className='navbar'>
-      <Link to='/home'>
-        <FontAwesomeIcon icon={faHome} className='home-icon' />
+      <Link to='/home' className='logo-container'>
+        <h5 className='logo'>Let's</h5>
+        <FontAwesomeIcon icon={faPlane} className='home-icon' />
+        <h5 className='logo'>Travel</h5>
       </Link>
       <div className='search-container'>
-        <input type='text' className='search' placeholder='        Search...'/>
+        <input type='text' className='search' placeholder='Search...'/>
         <FontAwesomeIcon icon={faSearch} className='search-icon' />
       </div>
       <Link to='/login'>
