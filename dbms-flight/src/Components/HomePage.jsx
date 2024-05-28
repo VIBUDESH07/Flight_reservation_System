a52039723c2b96aa2ff46f5c2ff8c278ea138098
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-28 23:50:07 +0530

    Update HomePage.jsx

--- dbms-flight/src/Components/HomePage.jsx
+++ dbms-flight/src/Components/HomePage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import logoImage from '../Styles/pics/malediven[1].webp'; // Import your image
+import '../Styles/Homepage.css';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <div className='image'>
+        <h1>Travel for enough, you meet yourself...</h1>
+        <img src={logoImage} alt='main' />
+      </div>
+      <div className='container'>
+
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
