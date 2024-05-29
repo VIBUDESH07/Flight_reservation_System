d95eddc97bfabdafd29cd638ac628f4a516d5218
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-29 11:06:47 +0530

    Update App.js

--- dbms-flight/src/App.js
+++ dbms-flight/src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
+import HomePage from "./Components/HomePage";
+import Addflight from "./Components/Addflight";
 
 function App() {
   return (
    <div>
-    <Layout/>
+
+    <Routes>
+      <Route path='/' element={<Layout/>}></Route>
+      <Route path='/add' element={<Addflight/>}></Route>
+    </Routes>
    </div>
   );
 }
