153d0b7a6ab528adc9343eb94918f2e39c4a8d0a
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-29 15:39:15 +0530

    Update App.js

--- dbms-flight/src/App.js
+++ dbms-flight/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./Components/Layout";
 import HomePage from "./Components/HomePage";
 import Addflight from "./Components/Addflight";
 import Login from "./Components/Login";
+import Fetch from "./Components/Fetch";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/' element={<Layout/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
       <Route path='/add' element={<Addflight/>}></Route>
+      <Route path='/fetch' element={<Fetch/>}></Route>
     </Routes>
    </div>
   );
