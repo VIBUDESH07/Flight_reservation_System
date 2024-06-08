accf4de62cc191be3909db935004a3336e808ff0
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-06-08 22:25:05 +0530

    Update Header.jsx

--- dbms-flight/src/Components/Header.jsx
+++ dbms-flight/src/Components/Header.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlane, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import '../Styles/header.css';
+ // Import the Sidebar component
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -47,11 +48,12 @@ const Header = () => {
   };
 
   return (
+
     <div className='navbar'>
       <Link to='/home' className='logo-container'>
-        <h5 className='logo'>Let's</h5>
+        <h5 className='logo1'>Let's</h5>
         <FontAwesomeIcon icon={faPlane} className='home-icon' />  
-        <h5 className='logo'>Travel</h5>
+        <h5 className='logo2'>Travel</h5>
       </Link>
       <div className='search-container'>
         <input type='text' className='search' placeholder='   Search...' />
