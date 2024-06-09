cda5ba6a1d052735818a3ce7723b7eaf35a3b619
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-06-09 13:27:57 +0530

    Update Header.jsx

--- dbms-flight/src/Components/Header.jsx
+++ dbms-flight/src/Components/Header.jsx
@@ -50,13 +50,13 @@ const Header = () => {
   return (
 
     <div className='navbar'>
-      <Link to='/home' className='logo-container'>
+      <Link to='/HomePage' className='logo-container'>
         <h5 className='logo1'>Let's</h5>
         <FontAwesomeIcon icon={faPlane} className='home-icon' />  
         <h5 className='logo2'>Travel</h5>
       </Link>
       <div className='search-container'>
-        <input type='text' className='search' placeholder='   Search...' />
+        <input type='text' className='search' placeholder='   Search for Flights' />
         <FontAwesomeIcon icon={faSearch} className='search-icon' />
       </div>
       <div className='btn'>
