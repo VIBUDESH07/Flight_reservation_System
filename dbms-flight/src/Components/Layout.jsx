0d215fe8550452859913f5b27368ef4225f17a07
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-09 09:14:17 +0530

    Update Layout.jsx

--- dbms-flight/src/Components/Layout.jsx
+++ dbms-flight/src/Components/Layout.jsx
@@ -1,15 +1,19 @@
-// Components/Layout.js
+// src/Components/Layout.jsx
 import React from 'react';
 import Header from './Header';
+import Sidebar from './Sidebar'; // Import Sidebar component
 import { Outlet } from 'react-router-dom';
+import '../Styles/Layout.css'; // Make sure to style the layout as needed
 
 const Layout = () => {
   return (
-    <div>
+    <div className="layout">
       <Header />
+    
+        <main className="main-content">
+          <Outlet /> {/* This is where the nested routes will be rendered */}
+        </main>
       
-        <Outlet /> {/* This is where the nested routes will be rendered */}
-
     </div>
   );
 }
